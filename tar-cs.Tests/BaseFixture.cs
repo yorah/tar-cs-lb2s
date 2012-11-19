b2f23830a3abc2f4b05ce1327217f02a08bc236b
yorah <yoram.harmelin@gmail.com>
2012-11-19 18:18:47 +0100

    Add test infrastructure using xunit
    Fixture classes must inherit from BaseFixture.
    Fixture methods can call `BuildOutStream()` to get a temporary stream
    The compilation mode `GenerateTestFiles` generates real tar files on the disk, instead of using in-memory streams.
    
    Most of the concepts regarding tests setup have been taken from https://github.com/libgit2/libgit2sharp/

---
+++ tar-cs.Tests/BaseFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace tar_cs.Tests
+{
+    public class BaseFixture
+    {
+        protected static string GeneratedFilesFolder
+        {
+            get { return @"GeneratedFiles"; }
+        }
+
+        static BaseFixture()
+        {
+            // Do the setup in the static ctor, so it only happens once.
+            // From https://github.com/libgit2/libgit2sharp/ (BaseFixture.cs)
+            var generatedFilesFolder = new DirectoryInfo(GeneratedFilesFolder);
+
+            if (generatedFilesFolder.Exists)
+            {
+                DirectoryHelper.DeleteSubdirectories(generatedFilesFolder.FullName);
+            }
+        }
+
+        protected Stream BuildOutStream()
+        {
+#if GENERATETESTFILES
+            var folder = new DirectoryInfo(GeneratedFilesFolder);
+            var testFolder = folder.CreateSubdirectory(new StackTrace(false).GetFrame(1).GetMethod().Name);
+
+            return new FileStream(Path.Combine(testFolder.FullName, Guid.NewGuid().ToString()) + ".tar", FileMode.Create);
+#else
+            return new MemoryStream();
+#endif
+        }
+    }
+}
---
+++ tar-cs.Tests/DirectoryHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace tar_cs.Tests
+{
+    // From https://github.com/libgit2/libgit2sharp/ (DirectoryHelper.cs)
+    public static class DirectoryHelper
+    {
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+        {
+            // From http://stackoverflow.com/questions/58744/best-way-to-copy-the-entire-contents-of-a-directory-in-c/58779#58779
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+            }
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
+
+        public static void DeleteSubdirectories(string parentPath)
+        {
+            string[] dirs = Directory.GetDirectories(parentPath);
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+        }
+
+        public static void DeleteDirectory(string directoryPath)
+        {
+            // From http://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true/329502#329502
+            string[] files = Directory.GetFiles(directoryPath);
+            string[] dirs = Directory.GetDirectories(directoryPath);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            File.SetAttributes(directoryPath, FileAttributes.Normal);
+            try
+            {
+                Directory.Delete(directoryPath, false);
+            }
+            catch (IOException ex)
+            {
+                throw new IOException(string.Format("{0}The directory '{1}' could not be deleted!" +
+                                                    "{0}Most of the time, this is due to an external process accessing the files in the temporary repositories created during the test runs, and keeping a handle on the directory, thus preventing the deletion of those files." +
+                                                    "{0}Known and common causes include:" +
+                                                    "{0}- Windows Search Indexer (go to the Indexing Options, in the Windows Control Panel, and exclude the bin folder of LibGit2Sharp.Tests)" +
+                                                    "{0}- Antivirus (exclude the bin folder of LibGit2Sharp.Tests from the paths scanned by your real-time antivirus){0}",
+                    Environment.NewLine, Path.GetFullPath(directoryPath)), ex);
+            }
+        }
+    }
+}
